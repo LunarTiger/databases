63ff75c414b207e3c11529bcb3151bb4fd5f70d4
LunarTiger <lunartiger@live.com>
2020-05-13 07:02:33 -0400

    :tada:

---
+++ index.js
@@ -0,0 +1,110 @@
+var homeConfig = {
+	databaseURL: "https://lunar-home.firebaseio.com/",
+};
+var home = firebase.initializeApp(homeConfig);
+var homeDatabase = home.database();
+//Event
+var event = homeDatabase.ref('stwl/next-event');
+event.on('value', (function(snapshot) {
+	var eventVal = snapshot.val();
+	if(eventVal){
+		document.getElementById('home-child').innerHTML += "<p>stwl/next-event:&nbsp; "+eventVal+"</p>";
+	}
+	if(!eventVal){
+		document.getElementById('home-child').innerHTML += "<p>stwl/next-event:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Book
+var book = homeDatabase.ref('stwl/book');
+book.on('value', (function(snapshot) {
+	var bookVal = snapshot.val();
+	if(bookVal){
+		document.getElementById('home-child').innerHTML += "<p>stwl/book:&nbsp; "+bookVal+"</p>";
+	}
+	if(!bookVal){
+		document.getElementById('home-child').innerHTML += "<p>stwl/book:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Location
+var loc = homeDatabase.ref('lunar/location');
+loc.on('value', (function(snapshot) {
+	var locVal = snapshot.val();
+	if(locVal){
+		document.getElementById('home-child').innerHTML += "<p>lunar/location:&nbsp; "+locVal+"</p>";
+	}
+	if(!locVal){
+		document.getElementById('home-child').innerHTML += "<p>lunar/location:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Toot
+var toot = homeDatabase.ref('mastodon/toot');
+toot.on('value', (function(snapshot) {
+	var tootVal = snapshot.val();
+	if(tootVal){
+		document.getElementById('home-child').innerHTML += "<p>mastodon/toot:&nbsp; "+tootVal+"</p>";
+	}
+	if(!tootVal){
+		document.getElementById('home-child').innerHTML += "<p>mastodon/toot:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Music
+var id = homeDatabase.ref('music-embed/id');
+id.on('value', (function(snapshot) {
+	var idVal = snapshot.val();
+	if(idVal){
+		document.getElementById('home-child').innerHTML += "<p>music-embed/id:&nbsp; "+idVal+"</p>";
+	}
+	if(!idVal){
+		document.getElementById('home-child').innerHTML += "<p>music-embed/id:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Post-Message
+var message = homeDatabase.ref('post-message/html');
+message.on('value', (function(snapshot) {
+	var messageVal = snapshot.val();
+	if(messageVal){
+		document.getElementById('home-child').innerHTML += "<p>post-message/html:&nbsp; "+messageVal+"</p>";
+	}
+	if(!messageVal){
+		document.getElementById('home-child').innerHTML += "<p>post-message/html:&nbsp; <mark>False</mark></p>";
+	}
+}));
+/////////////////////////////////////////////////////////////////
+var productionsConfig = {
+	databaseURL: "https://vr-productions-fce36.firebaseio.com/",
+};
+var productions = firebase.initializeApp(productionsConfig);
+var productionsDatabase = productions.database();
+//Rooftop Video
+var rooftop = productionsDatabase.ref('the-rooftop/id');
+rooftop.on('value', (function(snapshot) {
+	var rooftopVal = snapshot.val();
+	if(rooftopVal){
+		document.getElementById('productions-child').innerHTML += "<p>the-rooftop/id:&nbsp; "+rooftopVal+"</p>";
+	}
+	if(!rooftopVal){
+		document.getElementById('productions-child').innerHTML += "<p>the-rooftop/id:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//Open Mic Night Video
+var omn = productionsDatabase.ref('open-mic/id');
+omn.on('value', (function(snapshot) {
+	var omnVal = snapshot.val();
+	if(omnVal){
+		document.getElementById('productions-child').innerHTML += "<p>open-mic/id:&nbsp; "+omnVal+"</p>";
+	}
+	if(!omnVal){
+		document.getElementById('productions-child').innerHTML += "<p>open-mic/id:&nbsp; <mark>False</mark></p>";
+	}
+}));
+//The Hunt
+var hunt = productionsDatabase.ref('the-hunt/id');
+hunt.on('value', (function(snapshot) {
+	var huntVal = snapshot.val();
+	if(huntVal){
+		document.getElementById('productions-child').innerHTML += "<p>the-hunt/id:&nbsp; "+huntVal+"</p>";
+	}
+	if(!huntVal){
+		document.getElementById('productions-child').innerHTML += "<p>the-hunt/id:&nbsp; <mark>False</mark></p>";
+	}
+}));
--- index.md
+++ index.md
@@ -1,2 +1,13 @@
-<p>Please make a selection above.</p>
-<p>póg mo thóin</p>
+<div class="encase">
+	<button class="collapsible" id="home" data-parent="home" data-child="home-child">Lunar Home</button>
+		<div id="home-child" class="innertext" data-parent="home">
+		</div>
+	<button class="collapsible" id="productions" data-parent="productions" data-child="productions-child">4.0VR Productions</button>
+		<div id="productions-child" class="innertext" data-parent="productions">
+		</div>
+</div>
+<script src="https://www.gstatic.com/firebasejs/5.1.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.1.0/firebase-database.js"></script>
+<script src="index.js"></script>
+<!--<p>Please make a selection above.</p>
+<p>póg mo thóin</p>-->
